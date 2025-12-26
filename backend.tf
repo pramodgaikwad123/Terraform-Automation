ec3acaa5e7261c368cfaf11602e825cc5c555c2a
Pramod gaikwad <gaikwadgpramod369@gmail.com>
2025-12-26 09:13:23 +0530

    keypair add

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mydev-project-terraform-sample"
-    key = "newkeypairfile"
+    key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
