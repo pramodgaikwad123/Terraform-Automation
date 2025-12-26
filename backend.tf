c526e46b570d0312b8b8ea800a9eee3fdb47964b
Pramod gaikwad <gaikwadgpramod369@gmail.com>
2025-12-26 09:02:36 +0530

    s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "young-minds-app-project-terraform-state"
-    key = "main"
+    bucket = "mydev-project-terraform-sample"
+    key = "newkeypairfile"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
